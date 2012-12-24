45f3d43b3bff65ad5bc7270fb4977875ace73b3d
Scott Smith <scott@boundary.com>
2012-12-24 04:57:32 -0800

    update setup.py a bit

--- setup.py
+++ setup.py
@@ -2,26 +2,27 @@
 
 from setuptools import setup, find_packages
 
-version = '0.0.3'
+version = "0.0.3"
 
 setup(
-    name='logstash-tail',
+    name="logstash-tail",
+    url="http://github.com/ohlol/logstash-tail",
     version=version,
-    description='Tail logstash tcp output',
-    long_description='Tail logstash tcp output',
+    description="Tail logstash tcp output",
+    long_description="Tail logstash tcp output",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: BSD License',
-        'Topic :: System :: Networking :: Monitoring'
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: System :: Networking :: Monitoring"
     ],
-    keywords='',
-    author='Scott Smith',
-    author_email='scott@ohlol.net',
-    license='BSD',
+    keywords="",
+    author="Scott Smith",
+    author_email="scott@ohlol.net",
+    license="BSD",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['colorama'],
-    scripts=['bin/logstash-tail']
+    install_requires=["colorama"],
+    scripts=["bin/logstash-tail"]
 )
